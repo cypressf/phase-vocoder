65fe7438664c43df56f758274531a0e39b405407
Cypress Frankenfeld <iamcypress@gmail.com>
2014-11-20 17:54:30 -0500

    bug fixes

--- findPeaks4.py
+++ findPeaks4.py
@@ -6,7 +6,7 @@ def find_peaks4(amps, max_peak, eps_peak):
                      numpy.greater(amps[3:spectrum_size - 1], amps[4:spectrum_size]) * \
                      amps[3:spectrum_size - 1]
 
-    peak_pos = numpy.zeros(max_peak, 1)
+    peak_pos = numpy.transpose(numpy.zeros(max_peak))
     max_amp = max(peak_amplitude)
     n_peaks = 0
     for p in range(1, max_peak + 1):
