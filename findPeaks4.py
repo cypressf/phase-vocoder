a5880e507311cb414e990eb98f2b5b977f395e8e
Cypress Frankenfeld <iamcypress@gmail.com>
2014-11-20 16:48:58 -0500

    converted to python

--- findPeaks4.py
+++ findPeaks4.py
@@ -1,41 +1,38 @@
-function  peaks = findPeaks4( Amp, MAX_PEAK, EPS_PEAK, SSF )
+import numpy
 
-#   This version modified from findPeaks.m by P. Moller-Nielson 
-#   28.3.03, pm-n. ( see http://www.daimi.au.dk/~pmn/sound/ )
 
-SPECTRUM_SIZE=length(Amp)
-peakAmp = ( Amp(3:SPECTRUM_SIZE-1) > Amp(2:SPECTRUM_SIZE-2) ) .* ...
-          ( Amp(3:SPECTRUM_SIZE-1) > Amp(4:SPECTRUM_SIZE) ) .* ...
-          Amp(3:SPECTRUM_SIZE-1)
-peakPos = zeros( MAX_PEAK, 1)
+def find_peaks4(amps, max_peak, eps_peak):
+    spectrum_size = len(amps)
+    peak_amplitude = numpy.greater(amps[3:spectrum_size - 1], amps[2:spectrum_size - 2]) * \
+                     numpy.greater(amps[3:spectrum_size - 1], amps[4:spectrum_size]) * \
+                     amps[3:spectrum_size - 1]
 
-maxAmp = max( peakAmp )
-nPeaks = 0
-for p = 1 : MAX_PEAK
-  [m, b] = max( peakAmp )
-  if m <= ( EPS_PEAK * maxAmp )
-    break
-  end
-  peakPos(p) = b+2
-  peakAmp(b) = 0
-  nPeaks = p
-end
+    peak_pos = numpy.zeros(max_peak, 1)
+    max_amp = max(peak_amplitude)
+    n_peaks = 0
+    for p in range(1, max_peak + 1):
+        [m, b] = max(peak_amplitude)
+        if m <= ( eps_peak * max_amp ):
+            break
+        peak_pos[p] = b + 2
+        peak_amplitude[b] = 0
+        n_peaks = p
 
-peakPos = sort( peakPos )
-peaks = zeros( nPeaks, 3 )
+    peak_pos = numpy.sort(peak_pos)
+    peaks = numpy.zeros(n_peaks, 3)
 
-last_b = 1
-for p = 1 : nPeaks
-  b = peakPos(MAX_PEAK-nPeaks+p)
-  first_b = last_b+1
-  if p == nPeaks
-    last_b = SPECTRUM_SIZE
-  else
-    next_b = peakPos(MAX_PEAK-nPeaks+p+1)
-    [dummy, rel_min] = min( Amp(b:next_b))
-    last_b = b+rel_min-1
-  end
-  peaks(p,1) = first_b
-  peaks(p,2) = b
-  peaks(p,3) = last_b
-end
+    last_b = 1
+    for p in range(1, n_peaks + 1):
+        b = peak_pos[max_peak - n_peaks + p]
+        first_b = last_b + 1
+        if p == n_peaks:
+            last_b = spectrum_size
+        else:
+            next_b = peak_pos[max_peak - n_peaks + p + 1]
+            [dummy, rel_min] = min(amps[b:next_b])
+            last_b = b + rel_min - 1
+
+        peaks[p, 1] = first_b
+        peaks[p, 2] = b
+        peaks[p, 3] = last_b
+        return peaks
