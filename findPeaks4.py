e6f8e3be2f9b1e0cc645bc68ddcf9247fd47219c
Cypress Frankenfeld <iamcypress@gmail.com>
2014-12-09 16:59:32 -0500

    simplifications and bug fixes:     * simplified peak_positions (now a list of mutable size).     * renamed b to peak_position (because that's what it is)     * fixed the bug where argmin was looking at an empty slice

--- findPeaks4.py
+++ findPeaks4.py
@@ -9,34 +9,31 @@ def find_peaks4(spectrum, max_peaks, eps_peak):
     peak_amplitudes = greater_than_previous * greater_than_next * spectrum[3:-1]
 
     # identify the 50 biggest peaks, and save their indices in peak_positions
-    peak_positions = numpy.transpose(numpy.zeros(max_peaks))
+    peak_positions = []
     max_amp = max(peak_amplitudes)
-    n_peaks = 0
     for i in range(max_peaks):
-        b = numpy.argmax(peak_amplitudes)
-        m = peak_amplitudes[b]
+        peak_position = numpy.argmax(peak_amplitudes)
+        m = peak_amplitudes[peak_position]
         if m <= (eps_peak * max_amp):
             break
-        peak_positions[i] = b + 2
-        peak_amplitudes[b] = 0
-        n_peaks = i
+        peak_positions.append(peak_position + 2)
+        peak_amplitudes[peak_position] = 0
     peak_positions = numpy.sort(peak_positions)
 
     # get the peaks???
-    peaks = numpy.zeros([n_peaks, 3], dtype="uint32")
+    peaks = numpy.zeros([len(peak_positions), 3], dtype="uint32")
 
-    last_b = 1
-    for i in range(n_peaks):
-        b = peak_positions[max_peaks - n_peaks + i]
-        first_b = last_b + 1
-        if i == n_peaks:
-            last_b = len(spectrum)
+    previous_peak_position = 1
+    for i, peak_position in enumerate(peak_positions):
+        first_peak_position = previous_peak_position + 1
+        if i == len(peak_positions) - 1:
+            previous_peak_position = len(spectrum)
         else:
-            next_b = peak_positions[max_peaks - n_peaks + i]
-            rel_min = numpy.argmin(spectrum[b:next_b])
-            last_b = b + rel_min - 1
+            next_peak_position = peak_positions[i+1]
+            rel_min = numpy.argmin(spectrum[peak_position:next_peak_position])
+            previous_peak_position = peak_position + rel_min - 1
 
-        peaks[i, 0] = first_b
-        peaks[i, 1] = b
-        peaks[i, 2] = last_b
+        peaks[i, 0] = first_peak_position
+        peaks[i, 1] = peak_position
+        peaks[i, 2] = previous_peak_position
     return peaks
