65c3fcc819bd9b6592817378d0dd532ed77abd3b
Cypress Frankenfeld <iamcypress@gmail.com>
2014-12-09 16:23:23 -0500

    renamed variables to make it more readable.

--- findPeaks4.py
+++ findPeaks4.py
@@ -3,20 +3,20 @@ import numpy
 
 def find_peaks4(amps, max_peak, eps_peak):
     spectrum_size = len(amps)
-    peak_amplitude = numpy.greater(amps[3:spectrum_size - 1], amps[2:spectrum_size - 2]) * \
-                     numpy.greater(amps[3:spectrum_size - 1], amps[4:spectrum_size]) * \
-                     amps[3:spectrum_size - 1]
+    greater_than_previous = numpy.greater(amps[3:spectrum_size - 1], amps[2:spectrum_size - 2])
+    greater_than_next = numpy.greater(amps[3:spectrum_size - 1], amps[4:spectrum_size])
+    peak_amplitudes = greater_than_previous * greater_than_next * amps[3:spectrum_size - 1]
 
     peak_position = numpy.transpose(numpy.zeros(max_peak))
-    max_amp = max(peak_amplitude)
+    max_amp = max(peak_amplitudes)
     n_peaks = 0
     for p in range(max_peak):
-        b = numpy.argmax(peak_amplitude)
-        m = peak_amplitude[b]
+        b = numpy.argmax(peak_amplitudes)
+        m = peak_amplitudes[b]
         if m <= (eps_peak * max_amp):
             break
         peak_position[p] = b + 2
-        peak_amplitude[b] = 0
+        peak_amplitudes[b] = 0
         n_peaks = p
 
     peak_position = numpy.sort(peak_position)
