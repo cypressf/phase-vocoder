2e27324644c0b1847af35c3d5a7f4f43684fea3d
Cypress Frankenfeld <iamcypress@gmail.com>
2014-12-09 16:04:36 -0500

    renamed peak_pos to peak_position

--- findPeaks4.py
+++ findPeaks4.py
@@ -7,7 +7,7 @@ def find_peaks4(amps, max_peak, eps_peak):
                      numpy.greater(amps[3:spectrum_size - 1], amps[4:spectrum_size]) * \
                      amps[3:spectrum_size - 1]
 
-    peak_pos = numpy.transpose(numpy.zeros(max_peak))
+    peak_position = numpy.transpose(numpy.zeros(max_peak))
     max_amp = max(peak_amplitude)
     n_peaks = 0
     for p in range(max_peak):
@@ -15,21 +15,21 @@ def find_peaks4(amps, max_peak, eps_peak):
         m = peak_amplitude[b]
         if m <= (eps_peak * max_amp):
             break
-        peak_pos[p] = b + 2
+        peak_position[p] = b + 2
         peak_amplitude[b] = 0
         n_peaks = p
 
-    peak_pos = numpy.sort(peak_pos)
+    peak_position = numpy.sort(peak_position)
     peaks = numpy.zeros([n_peaks, 3], dtype="uint32")
 
     last_b = 1
     for p in range(n_peaks):
-        b = peak_pos[max_peak - n_peaks + p]
+        b = peak_position[max_peak - n_peaks + p]
         first_b = last_b + 1
         if p == n_peaks:
             last_b = spectrum_size
         else:
-            next_b = peak_pos[max_peak - n_peaks + p]
+            next_b = peak_position[max_peak - n_peaks + p]
             rel_min = numpy.argmin(amps[b:next_b])
             last_b = b + rel_min - 1
 
