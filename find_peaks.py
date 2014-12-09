9d2f24ca5f0e450fd8a7d832f7d2acdbe343d48e
Cypress Frankenfeld <iamcypress@gmail.com>
2014-12-09 18:02:04 -0500

    reduced index values of previous peak position by one

--- find_peaks.py
+++ find_peaks.py
@@ -23,17 +23,17 @@ def find_peaks(spectrum, max_peaks, eps_peak):
     # get the valleys in between the peaks or something?
     peaks = numpy.zeros([len(peak_positions), 3], dtype="uint32")
 
-    previous_peak_position = 1
+    previous_peak_position = 0 # TODO: verify that this index is correct
     for i, peak_position in enumerate(peak_positions):
         first_peak_position = previous_peak_position + 1
         if i == len(peak_positions) - 1:
-            previous_peak_position = len(spectrum)
+            previous_peak_position = len(spectrum) - 1 # TODO: verify that this index is correct
         else:
             next_peak_position = peak_positions[i+1]
             rel_min = numpy.argmin(spectrum[peak_position:next_peak_position])
             previous_peak_position = peak_position + rel_min - 1
 
-        peaks[i, 0] = first_peak_position #TODO: understand this
+        peaks[i, 0] = first_peak_position
         peaks[i, 1] = peak_position
-        peaks[i, 2] = previous_peak_position #TODO: understand this
+        peaks[i, 2] = previous_peak_position
     return peaks
