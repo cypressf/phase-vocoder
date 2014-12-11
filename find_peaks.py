63b9cda1cf94a5e5e6c39715454611a53d2874f2
Cypress Frankenfeld <iamcypress@gmail.com>
2014-12-11 17:26:31 -0500

    encapsulate more peak sorting logic in find_peaks.py

--- find_peaks.py
+++ find_peaks.py
@@ -1,12 +1,12 @@
 import numpy
 
 
-def find_peaks(spectrum, max_peaks, eps_peak):
+def find_peaks(spectrum_magnitude, max_peaks, eps_peak):
 
     # find the peaks in the spectrum
-    greater_than_previous = numpy.greater(spectrum[3:-1], spectrum[2:-2])
-    greater_than_next = numpy.greater(spectrum[3:-1], spectrum[4:])
-    peak_amplitudes = greater_than_previous * greater_than_next * spectrum[3:-1]
+    greater_than_previous = numpy.greater(spectrum_magnitude[3:-1], spectrum_magnitude[2:-2])
+    greater_than_next = numpy.greater(spectrum_magnitude[3:-1], spectrum_magnitude[4:])
+    peak_amplitudes = greater_than_previous * greater_than_next * spectrum_magnitude[3:-1]
 
     # identify the 50 biggest peaks, and save their indices in peak_positions
     peak_positions = []
@@ -23,17 +23,19 @@ def find_peaks(spectrum, max_peaks, eps_peak):
     # get the valleys in between the peaks or something?
     peaks = numpy.zeros([len(peak_positions), 3], dtype="uint32")
 
-    previous_peak_position = 0 # TODO: verify that this index is correct
+    previous_peak_position = 0  # TODO: verify that this index is correct
     for i, peak_position in enumerate(peak_positions):
         first_peak_position = previous_peak_position + 1
         if i == len(peak_positions) - 1:
-            previous_peak_position = len(spectrum) - 1 # TODO: verify that this index is correct
+            previous_peak_position = len(spectrum_magnitude) - 1  # TODO: verify that this index is correct
         else:
             next_peak_position = peak_positions[i+1]
-            rel_min = numpy.argmin(spectrum[peak_position:next_peak_position])
+            rel_min = numpy.argmin(spectrum_magnitude[peak_position:next_peak_position])
             previous_peak_position = peak_position + rel_min - 1
 
         peaks[i, 0] = first_peak_position
         peaks[i, 1] = peak_position
         peaks[i, 2] = previous_peak_position
-    return peaks
+
+    peak_values = spectrum_magnitude[peaks[:, 1]]
+    return peaks[numpy.argsort(peak_values), :]
